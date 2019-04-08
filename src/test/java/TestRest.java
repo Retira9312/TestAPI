5dfd4989d264c4cc07e5a5e8f7cdbd5d01695d76
Retira9312 <48682202+Retira9312@users.noreply.github.com>
2019-04-08 15:45:01 +0700

    Add files via upload

--- src/test/java/TestRest.java
+++ src/test/java/TestRest.java
@@ -5,8 +5,11 @@ import io.restassured.response.Response;
 import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
 import org.json.JSONException;
+import org.junit.After;
+import org.junit.Before;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 
 
 import static io.restassured.RestAssured.given;
@@ -18,15 +21,16 @@ public class TestRest {
 
     @Test(description = "API GET REQUEST TEST")
     public <string> void getRequestExampleTest() throws JSONException {
-
-        RestAssured.baseURI = "https://api.vk.com/method/users.get?user_id=20723173&access_token=4d3f7b9fb2e866be5146d3b9dbdd6a828a56bff67995e6acc02dd464970e10c1ac585a5aaa2e373e39777&v=5.92";
          RequestSpecification httpRequest = given();
-         Response response = httpRequest.get("/id");
-         ResponseBody body = response.getBody();
-         String bodyAsString = body.asString();
-         Assert.assertTrue(bodyAsString.contains("id"), "Response body contains Hyderabad");
+         Response response = httpRequest.get("https://api.vk.com/method/users.get?user_id=20723173&access_token=a4f28633b93b1e64f0bc6dac52fd46289812f1a766dcaaa22bdf8f09b75aff92cc8ef6bdbae653410dd31&v=5.92");
          response.prettyPrint();
 
+         Response responseF = httpRequest.get("https://api.vk.com/method/friends.get?user_id=20723173&access_token=a4f28633b93b1e64f0bc6dac52fd46289812f1a766dcaaa22bdf8f09b75aff92cc8ef6bdbae653410dd31&v=5.92");
+         responseF.prettyPrint();
+
+
+         Response responseS = httpRequest.get("https://api.vk.com/method/status.set?group_id=117179357&text='text'&access_token=39bad999230de9ed0e203e93c260db123f9b7fc5bfaca8baf2d360ae603f74af4f60a6d3a01b5ff41dcb0&v=5.92");
+         responseS.prettyPrint();
 
 
 
